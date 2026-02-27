d79693d3d38f61f1e6b3ad13c197b75fb4fdd9df
Elliott Amador <125588898+elliottamador@users.noreply.github.com>
2026-02-27 10:48:34 -0800

    feat: update product data with full 270 products

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,4 +1,1 @@
-export const products = [
-  {"sku":3849012,"name":"Sonance - Sonamp 2-100 Amplifier","model":"SONAMP 2-100","regularPrice":990,"salePrice":742.99,"onSale":true,"category":"Power Amplifiers","inStore":true,"online":true,"rating":4.6,"reviewCount":7,"color":"Black","url":"https://api.bestbuy.com/click/-/3849012/pdp"}
-] as const;
-export type Product = typeof products[number];
+PLACEHOLDER_WILL_USE_PUSH_FILES
