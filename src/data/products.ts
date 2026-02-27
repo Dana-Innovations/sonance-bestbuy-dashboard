b11e1b2a5d910e4fe5c5fc8aada4b6d8175dcbfa
Elliott Amador <125588898+elliottamador@users.noreply.github.com>
2026-02-27 10:53:39 -0800

    feat: add full product data (270 products in 5 chunks)

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,1 +1,8 @@
-PLACEHOLDER_WILL_USE_PUSH_FILES
+import { products1 } from "./products1";
+import { products2 } from "./products2";
+import { products3 } from "./products3";
+import { products4 } from "./products4";
+import { products5 } from "./products5";
+
+export const products = [...products1, ...products2, ...products3, ...products4, ...products5] as const;
+export type Product = typeof products[number];
